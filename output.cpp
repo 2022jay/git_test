36e544384ab15d28b79603f4bce19d41c4fc4d93
JAY2024 <419849415@qq.com>
2024-07-26 10:38:01 +0800

    no message

---
+++ output.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a = 1;
+	float b = 2.1;
+	double c= 2.111;
+	char arr[10] = { 0 };
+	char d[] = "hello world";
+	cin >> arr;
+	cout << arr << endl;
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << d << endl;
+	return 0;
+}
